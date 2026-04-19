439b346448fe2c0da131acba387aab22e96992db
Andrius <andrius.dobravolskas@gmail.com>
2026-04-19 17:05:01 +0300

    SCRUM-128: Add add_funds() alias to BalanceManager

--- Scripts/BalanceManager.gd
+++ Scripts/BalanceManager.gd
@@ -1,28 +1,21 @@
 extends Node
-
-## BalanceManager - Global singleton for managing player balance
-## SCRUM-99: Create script to store player balance
-## SCRUM-100: Autoload singleton
-## SCRUM-102: var balance: int = 1000
-
 var balance: int = 1000
-
 signal balance_changed(new_balance: int)
-
 
 func get_balance() -> int:
 	return balance
-
 
 func set_balance(value: int) -> void:
 	balance = max(0, value)
 	balance_changed.emit(balance)
 
-
 func add_balance(amount: int) -> void:
 	balance += amount
 	balance_changed.emit(balance)
 
+# SCRUM-129: add_funds alias for sprint compatibility
+func add_funds(amount: int) -> void:
+	add_balance(amount)
 
 func subtract_balance(amount: int) -> bool:
 	if amount > balance:
@@ -31,10 +24,8 @@ func subtract_balance(amount: int) -> bool:
 	balance_changed.emit(balance)
 	return true
 
-
 func can_afford(amount: int) -> bool:
 	return balance >= amount
-
 
 func reset_balance() -> void:
 	balance = 1000
